ea578d1aeb3a997277d4f4bba9d837a433ca83b5
jesuspa17 <jpallares94.jp@gmail.com>
2015-11-25 22:27:12 +0100

    Primera subida

---
+++ ClienteAEMET/app/src/main/java/com/dam/adpspsalesianostriana/clienteaemet/Pojo_AEMET/Dia.java
@@ -0,0 +1,106 @@
+package com.dam.adpspsalesianostriana.clienteaemet.Pojo_AEMET;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Jesús Pallares on 20/11/2015.
+ */
+@Root(strict = false)
+public class Dia {
+
+    @Attribute(name="fecha")
+    private Date fecha;
+
+    @Element(name="temperatura")
+    private Temperatura temperatura;
+
+    @ElementList(name = "cota_nieve_prov", inline = true)
+    private List<Cota_nieve_prov> cota_nieve_prov;
+
+    @ElementList(name = "prob_precipitacion", inline = true)
+    private List<Prob_precipitacion> prob_precipitacion;
+
+    @ElementList(name = "estado_cielo", inline = true)
+    private List<Estado_cielo> estado_cielo;
+
+    @ElementList(name = "sens_termica", inline = true)
+    private List<Sens_termica> sens_termica;
+
+
+    public Dia(){}
+
+    public Dia(Date fecha, Temperatura temperatura, List<Cota_nieve_prov> cota_nieve_prov, List<Prob_precipitacion> prob_precipitacion, List<Estado_cielo> estado_cielo, List<Sens_termica> sens_termica) {
+        this.fecha = fecha;
+        this.temperatura = temperatura;
+        this.cota_nieve_prov = cota_nieve_prov;
+        this.prob_precipitacion = prob_precipitacion;
+        this.estado_cielo = estado_cielo;
+        this.sens_termica = sens_termica;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Temperatura getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(Temperatura temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public List<Cota_nieve_prov> getCota_nieve_prov() {
+        return cota_nieve_prov;
+    }
+
+    public void setCota_nieve_prov(List<Cota_nieve_prov> cota_nieve_prov) {
+        this.cota_nieve_prov = cota_nieve_prov;
+    }
+
+    public List<Prob_precipitacion> getProb_precipitacion() {
+        return prob_precipitacion;
+    }
+
+    public void setProb_precipitacion(List<Prob_precipitacion> prob_precipitacion) {
+        this.prob_precipitacion = prob_precipitacion;
+    }
+
+    public List<Estado_cielo> getEstado_cielo() {
+        return estado_cielo;
+    }
+
+    public void setEstado_cielo(List<Estado_cielo> estado_cielo) {
+        this.estado_cielo = estado_cielo;
+    }
+
+    public List<Sens_termica> getSens_termica() {
+        return sens_termica;
+    }
+
+    public void setSens_termica(List<Sens_termica> sens_termica) {
+        this.sens_termica = sens_termica;
+    }
+
+    @Override
+    public String toString() {
+        return "Dia{" +
+                "fecha=" + fecha +
+                ", temperatura=" + temperatura +
+                ", cota_nieve_prov=" + cota_nieve_prov +
+                ", prob_precipitacion=" + prob_precipitacion +
+                ", estado_cielo=" + estado_cielo +
+                ", sens_termica=" + sens_termica +
+                '}';
+    }
+}
---
+++ ClienteAEMET/app/src/main/java/com/dam/adpspsalesianostriana/clienteaemet/SimpleXMLRequest.java
@@ -0,0 +1,76 @@
+package com.dam.adpspsalesianostriana.clienteaemet;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+
+/**
+ * Created by Luismi on 20/11/2015.
+ */
+public class SimpleXMLRequest<T> extends Request<T> {
+
+    private final Serializer serializer = new Persister();
+    private final Class<T> clazz;
+    private final Map<String, String> headers;
+    private final Response.Listener<T> listener;
+
+    /**
+     * Make a GET request and return a parsed object from JSON.
+     *
+     * @param url     URL of the request to make
+     * @param clazz   Relevant class object, for Gson's reflection
+     * @param headers Map of request headers
+     */
+    public SimpleXMLRequest(String url, Class<T> clazz, Map<String, String> headers,
+                            Response.Listener<T> listener, Response.ErrorListener errorListener) {
+        super(Method.GET, url, errorListener);
+        //Aumentamos el tiempo de espera de la conexión,
+        //sustituyendo la RetryPolicy por una propia
+        setRetryPolicy(new DefaultRetryPolicy(5000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        this.clazz = clazz;
+        this.headers = headers;
+        this.listener = listener;
+    }
+
+
+
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        return headers != null ? headers : super.getHeaders();
+    }
+
+    @Override
+    protected void deliverResponse(T response) {
+        listener.onResponse(response);
+    }
+
+    @Override
+    protected Response<T> parseNetworkResponse(NetworkResponse response) {
+        try {
+
+            String xml = new String(
+                    response.data,
+                    //HttpHeaderParser.parseCharset(response.headers));
+                    Charset.forName("utf-8"));
+
+            return Response.success(
+                    serializer.read(clazz, xml, false),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (Exception e) {
+            return Response.error(new ParseError(e));
+        }
+
+    }
+}
