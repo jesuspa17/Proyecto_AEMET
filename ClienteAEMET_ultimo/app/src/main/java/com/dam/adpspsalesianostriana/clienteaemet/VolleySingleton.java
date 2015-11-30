1f5f1070f2af29ec8e8be6ba28e61c9fd07ab2f2
jesuspa17 <jpallares94.jp@gmail.com>
2015-12-01 00:27:29 +0100

    Ultima subida
    -Se permite guardar en preferencias la última busqueda realizada.
    -Se controlan los errores de busqueda

---
+++ ClienteAEMET_ultimo/app/src/main/java/com/dam/adpspsalesianostriana/clienteaemet/VolleySingleton.java
@@ -0,0 +1,34 @@
+package com.dam.adpspsalesianostriana.clienteaemet;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleySingleton {
+
+    private static VolleySingleton mInstance;
+    private Context mContext;
+    private RequestQueue mRequestQueue;
+
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
+    }
+
+    private VolleySingleton(Context _mContext) {
+
+        mContext = _mContext;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+}
